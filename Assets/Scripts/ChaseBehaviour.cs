cb5a696bd2e1f02fc3758257ea061a95dc77a758
pacter-69 <aldomcorb@gmail.com>
2026-02-07 02:55:55 +0100

    Enemy detect near player and angle range

--- Assets/Scripts/ChaseBehaviour.cs
+++ Assets/Scripts/ChaseBehaviour.cs
@@ -7,9 +7,12 @@ public class ChaseBehaviour : MonoBehaviour
     [SerializeField]
     private float distance;
 
-    public float speed = 2;
     public float visionRange;
-    public bool isPlayerClose { get; private set; }
+    public float visionAngle;
+
+    public bool isPlayerClose;
+    public bool isPlayerOnAngle;
+    public float angle;
 
     private Transform player;
 
@@ -17,6 +20,9 @@ public class ChaseBehaviour : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         isPlayerClose = IsPlayerClose(transform);
+
+        if (isPlayerClose) isPlayerOnAngle = IsPlayerOnAngle(transform);
+        else isPlayerOnAngle = false;
     }
 
     private bool IsPlayerClose(Transform transform)
@@ -26,8 +32,16 @@ public class ChaseBehaviour : MonoBehaviour
         return (dist < visionRange);
     }
 
+    private bool IsPlayerOnAngle(Transform transform)
+    {
+        return Vector2.Angle(transform.right, player.position - transform.position) < visionAngle;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, visionRange);
+        Gizmos.DrawRay(transform.position, Quaternion.AngleAxis(visionAngle, transform.forward) * transform.right * visionRange);
+        Gizmos.DrawRay(transform.position, Quaternion.AngleAxis(-visionAngle, transform.forward) * transform.right * visionRange);
+        if(player != null) Gizmos.DrawLine(transform.position, player.position);
     }
 }
