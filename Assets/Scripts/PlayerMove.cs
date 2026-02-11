1f28d18e405b8c798a0d42f96091a212bb37fb51
Raúl <romeromorerraul@gmail.com>
2026-02-11 19:28:19 +0100

    Cambiando el movimiento su nombre por si acaso

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,29 +8,29 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private float speed = 5.0f;
 
-    Rigidbody2D _rigidbody;
-    private float _horizontalDir, _verticalDir;
+    Rigidbody2D body;
+    private float horizontalDir, verticalDir;
 
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        Vector2 velocity = _rigidbody.linearVelocity;
-        _rigidbody.linearVelocity = new Vector2(_horizontalDir, _verticalDir).normalized * speed;
+        Vector2 velocity = body.linearVelocity;
+        body.linearVelocity = new Vector2(horizontalDir, verticalDir).normalized * speed;
     }
 
     void OnMoveX(InputValue value)
     {
         var inputVal = value.Get<Vector2>();
-        _horizontalDir = inputVal.x;
+        horizontalDir = inputVal.x;
     }
 
     void OnMoveY(InputValue value)
     {
         var inputVal = value.Get<Vector2>();
-        _verticalDir = inputVal.y;
+        verticalDir = inputVal.y;
     }
 }
