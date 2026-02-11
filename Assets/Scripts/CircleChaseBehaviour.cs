5d55700044e0f3b4f67ea53f811373a02d767c6c
pacter-69 <aldomcorb@gmail.com>
2026-02-11 17:36:26 +0100

    Updated level design

--- Assets/Scripts/CircleChaseBehaviour.cs
+++ Assets/Scripts/CircleChaseBehaviour.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class CircleChaseBehaviour : StateMachineBehaviour
 {
-    
+    public float speed;
+    public float visionRange;
+
+    private Transform player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var playerClose = IsPlayerClose(animator.transform);
+        animator.SetBool("isChasing", playerClose);
+
+        Vector2 dir = player.position - animator.transform.position;
+        animator.transform.right = dir.normalized;
+        animator.transform.position += (Vector3)dir.normalized * speed * Time.deltaTime;
+    }
+
+    private bool IsPlayerClose(Transform transform)
+    {
+        var dist = Vector3.Distance(transform.position, player.position);
+        return (dist < visionRange);
+    }
 }
